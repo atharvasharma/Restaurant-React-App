a660522abf642ebff29ef35b59537713bf22b149
Atharva Sharma <atharvasjp1@gmail.com>
2020-05-14 00:30:01 +0530

    Updates

---
+++ precache-manifest.55813c65580ec7ad0549fe17e942e964.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c45d2ee34759bb10febc67a3f183af6",
+    "url": "/Restaurant-React-App/index.html"
+  },
+  {
+    "revision": "8fc57deca962a9f70196",
+    "url": "/Restaurant-React-App/static/css/2.5653e56d.chunk.css"
+  },
+  {
+    "revision": "befc235192b1782902e6",
+    "url": "/Restaurant-React-App/static/css/main.8b54a20d.chunk.css"
+  },
+  {
+    "revision": "8fc57deca962a9f70196",
+    "url": "/Restaurant-React-App/static/js/2.00ea3f5f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Restaurant-React-App/static/js/2.00ea3f5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "befc235192b1782902e6",
+    "url": "/Restaurant-React-App/static/js/main.5af5a39f.chunk.js"
+  },
+  {
+    "revision": "a8f38bc47173b96e0068",
+    "url": "/Restaurant-React-App/static/js/runtime-main.b69ba827.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Restaurant-React-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Restaurant-React-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Restaurant-React-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Restaurant-React-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Restaurant-React-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
